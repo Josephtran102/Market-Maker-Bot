46eb3ab7c88846d02d1fb722269831e625ef0a93
Josephtran102 <josephtran0505@gmail.com>
2023-11-25 13:40:08 +0700

    Commit message

--- weth-sweth.js
+++ weth-sweth.js
@@ -5,13 +5,13 @@ const wethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'; // goerli weth
 //const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // mainnet weth
 const routerAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564'; // Uniswap Router
 const quoterAddress = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6'; // Uniswap Quoter
-const tokenAddress = '0x07865c6e87b9f70255377e024ace6630c1eaa37f'; // goerli USDC
+const tokenAddress = '0x178e141a0e3b34152f73ff610437a7bf9b83267a'; // goerli USDC
 const fee = 500; // Uniswap pool fee bps 500, 3000, 10000
-const buyAmount = ethers.parseUnits('0.0000045', 'ether');
-const targetPrice = BigInt(100); // target exchange rate: *với 0.0000045 WETH sẽ mua được 100 USDC
+const buyAmount = ethers.parseUnits('0.1', 'ether');
+const targetPrice = BigInt(100506); // target exchange rate: *với 0.0000045 WETH sẽ mua được 100 USDC
 const targetAmountOut = buyAmount * targetPrice;
 const sellAmount = buyAmount / targetPrice;
-const tradeFrequency = 1 * 60 * 1000; // ms (once per hour)
+const tradeFrequency = 3600 * 1000; // ms (once per hour)
 
 // `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
 const provider = new ethers.JsonRpcProvider(`https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
